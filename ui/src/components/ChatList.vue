52a7411cdb428d9bb27366c6427278cb64286141
vanderkilu <vanderkiliku@gmail.com>
2019-02-01 20:35:52 +0000

    update ui

--- ui/src/components/ChatList.vue
+++ ui/src/components/ChatList.vue
@@ -36,7 +36,7 @@ export default {
     },
     methods: {
          randomPos() {
-            return Math.floor(Math.random() * 60* Math.random() * 5) + '%'
+            return Math.floor(Math.random() * 50) + '%'
         },
         sendMessage() {
             if (this.message === '') {

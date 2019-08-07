9fd6409fc7178535bfef82766de0ea11d73ddb0d
vanderkilu <vanderkiliku@gmail.com>
2019-08-07 17:34:09 +0000

    remove date

--- ui/src/service.js
+++ ui/src/service.js
@@ -3,12 +3,5 @@ const key= 'vChat-username'
 const storeUsername = (username) => localStorage.setItem(key, username)
 const getUsername = () => localStorage.getItem(key)
 
-const isDue = (prev, cur) => {
-   const scale = 1000 * 10
-   const elapse = cur - new Date(prev)
-   return elapse >= scale
-}
-
-
-export {storeUsername, getUsername, isDue}
+export {storeUsername, getUsername}
 
